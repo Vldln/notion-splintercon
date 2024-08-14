612f8cd9e09d0c0c8041206a8d83e2297d09e50f
Vldln <joker5916@gmail.com>
2024-08-14 13:58:37 +0400

    add: rpototype

--- app.vue
+++ app.vue
@@ -1,48 +1,14 @@
 <template>
-  <div>
-    <h1>Submitted Data</h1>
-    <div v-if="data.length">
-      <div v-for="item in data" :key="item.id">
-        <div v-if="item.properties.Select?.select?.name === 'Publish'">
-          <p>
-            <strong>Name:</strong>
-            {{ item.properties.Name.title[0]?.plain_text }}
-          </p>
-          <p>
-            <strong>Descr:</strong>
-            {{ item.properties.Descr.rich_text[0]?.plain_text }}
-          </p>
-        </div>
-      </div>
-    </div>
-    <div v-else>
-      <p>No data submitted yet.</p>
-    </div>
-  </div>
+  <Html>
+    <Body
+      class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
+    >
+      <NuxtLayout>
+        <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
 </template>
-
 <script setup>
-import { ref, onMounted, onUnmounted } from "vue";
-import axios from "axios";
-
-const data = ref([]);
-
-async function fetchData() {
-  try {
-    const response = await axios.get("/api/database");
-    data.value = response.data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-}
-
-onMounted(() => {
-  fetchData();
-  const interval = setInterval(fetchData, 5000);
-
-  // Clean up the interval when the component is unmounted
-  onUnmounted(() => {
-    clearInterval(interval);
-  });
-});
 </script>
---
+++ pages/apply.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Apply Form</h1>
+    <form @submit.prevent="submitForm">
+      <div>
+        <label for="name">Name</label>
+        <input v-model="form.name" id="name" type="text" required />
+      </div>
+      <div>
+        <label for="descr">Email</label>
+        <input v-model="form.descr" id="descr" type="text" required />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+  
+  <script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const form = ref({
+  name: "",
+  descr: "",
+});
+
+const submitForm = async () => {
+  try {
+    await axios.post("/api/database", form.value);
+    alert("Form submitted successfully!");
+  } catch (error) {
+    console.error("Error submitting form:", error);
+    alert("There was an error submitting the form.");
+  }
+};
+</script>
+  
