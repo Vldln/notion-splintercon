a4b06a8eaf6e00c78c510120e669ad9bf32e0932
Vldln <joker5916@gmail.com>
2024-08-26 17:55:40 +0400

    fix: dev server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  devServer: {
+    port: 5000,
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"]
 })
