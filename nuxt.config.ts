1a7f432d025f4620be9416f4b81ed59463a3efe9
Vldln <joker5916@gmail.com>
2024-09-23 14:18:09 +0400

    add: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
   devServer: {
     port: 5000,
   },
